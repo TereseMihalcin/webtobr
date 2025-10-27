cf7df65f2f13554efddb7f0aa4eb71d001bd435d
Terese Mihalcin <teresemihalcin18@gmail.com>
2025-10-26 20:03:49 -0400

    heading and cleanup

--- src/assets/counter.js
+++ src/assets/counter.js
@@ -1,19 +1,7 @@
-
-console.log('test');
-
-
 var ahri = '../assets/sitting.png';
-console.log('test');
-// const imageContainer = document.getElementById("seconds");
-// console.log("trs"+imageContainer);
-// imageContainer.innerHTML = `<img src=${ahri} alt="sitting ahri">`;
-
 let images = ['/sitting.png', '/meow.png', '/cross-legged.png', '/crazy.png'];
-
 let index = 0;
 const imgElement = document.querySelector('#seconds');
-
-// console.log(imgElement);
 
 function change() {
   imgElement.src = images[index];
